3f91079a9c9db09562791b4970f4662390ef780e
Nisha Ingole <ingolenisha9@gmail.com>
2026-02-28 09:37:03 +0530

    url changes

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -25,7 +25,8 @@ function Login() {
       return handleError("All fields are required");
     }
     try {
-      const url = "http://localhost:8080/auth/login";
+      const url =
+        "https://authentication-system-1-k2uf.onrender.com/auth/login";
       const response = await fetch(url, {
         method: "POST",
         headers: {
